f6a40d95edb4e77d7c78267c36efcb91c64dbdf0
mony <mony.bd@asa-international.com>
2021-06-24 19:10:56 +0600

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WebApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+                //Arrange:Initialize objects. Creates an instance of the class
+                UnitTestClass tp = new UnitTestClass();
+                //assert part
+                Assert.AreEqual(tp.Name, "Unit Testing");
+            
+        }
+    }
+}
